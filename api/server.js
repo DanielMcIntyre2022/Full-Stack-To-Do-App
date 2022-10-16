7cd411efa345e8b546aab77f3abd062977c7930b
Daniel <daniel-mcintyre@hotmail.com>
2022-10-16 17:55:59 -0400

    added todo model to server file and create post request for new todos

--- api/server.js
+++ api/server.js
@@ -8,6 +8,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to DB')).catch(console.error);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected to DB'))
+.catch(console.error);
+
+const Todo = require('./models/Todo');
+
+app.get('/todos', async(req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save();
+
+    res.json(todo);
+});
 
 app.listen(3001, ()=> console.log('server started on port 3001'));
